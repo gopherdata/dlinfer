228f3dd49485dd835ae48d24904609f9e6c3cd36
Daniel Whitenack <whitenack.daniel@gmail.com>
2017-01-04 15:00:09 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# A deep learning inference engine for Go
+
+This project intends to make trained models, such as AlexNet, GoogleNet, etc., accessible within Go.  It utilizes the functionality of Intel's inference engine (part of the deep learning SDK) to interact with trained and optimized neural networks. 
+
+This is work in progress.  An official release is forthcoming.
+
---
+++ dlinfer.go
@@ -0,0 +1,27 @@
+package dlinfer
+
+// Configurator includes the necessary pieces of an
+// Inferenence Engine Configurator, as used in the Intel
+// Deep Learning SDK.
+type Configurator struct {
+}
+
+// Results contains the results of an inference.
+type Results struct {
+}
+
+// LoadImage loads an image as input to an inference.
+func LoadImage() {
+
+}
+
+// Infer infers results based on an input image.
+func Infer() {
+
+}
+
+// GetTopResult gets the top results from the general
+// results produced by an inference.
+func GetTopResult() {
+
+}
