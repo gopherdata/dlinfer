086df83c30cceebe170755692b58e88491cbdc3e
Daniel Whitenack <whitenack.daniel@gmail.com>
2017-02-18 06:46:08 -0500

    changes to example

--- examples/basic_classification/main.go
+++ examples/basic_classification/main.go
@@ -6,13 +6,18 @@ import (
 	"github.com/gopherds/dlinfer"
 )
 
+const (
+	model      = "../../../../dwhitena/intel_sdk_test/alexnet/model/CaffeNet.xml"
+	pluginPath = "/opt/intel/deep_learning_sdk_2016.1.0.861/deployment_tools/inference_engine/lib/intel64/"
+	plugin     = "MKLDNNPlugin"
+	labelsFile = "../../../../dwhitena/intel_sdk_test/alexnet/model/CaffeNet.labels"
+)
+
 func main() {
 
 	// Create an inference configurator value.
-	model := ""
-	pluginPaths := []string{""}
-	plugin := ""
-	labelsFile := ""
+	pluginPaths := dlinfer.NewStringVector()
+	pluginPaths.Add(pluginPath)
 	configurator := dlinfer.NewInferenceEngineConfigurator(model, pluginPaths, plugin, labelsFile)
 
 	fmt.Println(configurator)
