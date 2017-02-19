e85d2e35aab63d5733fcc4f86b9d17a86c8e506c
Daniel Whitenack <whitenack.daniel@gmail.com>
2017-02-19 06:37:44 -0500

    update image as input on command line

--- examples/basic_classification/main.go
+++ examples/basic_classification/main.go
@@ -1,16 +1,27 @@
 package main
 
-import "github.com/gopherds/dlinfer"
+import (
+	"log"
+	"os"
+
+	"github.com/gopherds/dlinfer"
+)
 
 const (
 	model      = "/CaffeNet.xml"
 	pluginPath = "/opt/intel/deep_learning_sdk_2016.1.0.861/deployment_tools/inference_engine/lib/intel64/"
 	plugin     = "MKLDNNPlugin"
 	labelsFile = "/CaffeNet.labels"
-	imageFile  = "/cat.bmp"
 )
 
 func main() {
+
+	// Read in the image file name.
+	args := os.Args
+	if len(args) != 2 {
+		log.Fatal("Please provide an image as input")
+	}
+	imageFile = args[1]
 
 	// Create an inference configurator value.
 	pluginPaths := dlinfer.NewStringVector()
